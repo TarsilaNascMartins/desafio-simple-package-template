c6606afe03e533f75e9836337f3a7b71c697a7ae
TarsilaNascMartins <tarsilawho@gmail.com>
2022-11-10 22:11:07 -0300

    3

--- setup.py
+++ setup.py
@@ -1,21 +1,45 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
-    page_description = f.read()
+# with open("README.md", "r") as f:
+#     page_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+# with open("requirements.txt") as f:
+#     requirements = f.read().splitlines()
 
-setup(
-    name="package_aumentosalario",
-    version="0.0.1",
-    author="Tarsila Martins",
+# setup(
+#     name="package_aumentosalario",
+#     version="0.0.1",
+#     author="Tarsila Martins",
+#     author_email="tarsilawho@gmail.com",
+#     description="My short description",
+#     long_description=page_description,
+#     long_description_content_type="text/markdown",
+#     url='https://github.com/TarsilaNascMartins/desafio-simple-package-template',
+#     packages=find_packages(),
+#     install_requires=requirements,
+#     python_requires='>=3.8',
+   
+# )
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="libradocs", # Replace with your own username
+    version="0.0.0",
+    author="Tarsila Nascimento",
     author_email="tarsilawho@gmail.com",
-    description="My short description",
-    long_description=page_description,
+    description="A small example package",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/TarsilaNascMartins/desafio-simple-package-template",
-    packages=find_packages(),
-    install_requires=requirements,
+    homepage="https://github.com/TarsilaNascMartins/desafio-simple-package-template",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     python_requires='>=3.8',
 )
